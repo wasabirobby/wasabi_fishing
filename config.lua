205de8d6aa9b14086e25423850c0b7bd45c5cc26
wasabirobby <79250053+wasabirobby@users.noreply.github.com>
2022-09-30 16:32:13 -0700

    Update config.lua

--- config.lua
+++ config.lua
@@ -1,5 +1,5 @@
 -----------------For support, scripts, and more----------------
------------------https://discord.gg/XJFNyMy3Bv-----------------
+--------------- https://discord.gg/wasabiscripts  -------------
 ---------------------------------------------------------------
 local seconds, minutes = 1000, 60000
 Config = {}
