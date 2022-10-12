bb2a1dd2f06af9f9bd085bf0c7cb1628f389f1aa
wasabirobby <79250053+wasabirobby@users.noreply.github.com>
2022-10-12 09:28:15 -0700

    revert: remove auto insert sql

---
+++ sql/wasabi_fishing_limit.sql
@@ -0,0 +1,8 @@
+INSERT INTO `items` (`name`, `label`, `limit`) VALUES
+	('fishingrod', 'Fishing Rod', 20),
+	('fishbait', 'Fish Bait', 20),
+	('anchovy', 'Anchovy', 20),
+	('trout', 'Trout', 20),
+	('salmon', 'Salmon', 20),
+	('tuna', 'Tuna', 40)
+;
