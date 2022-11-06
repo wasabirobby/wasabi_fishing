31f18a3d42ed05dc0f4bd16a745bc0c541d782a1
wasabirobby <79250053+wasabirobby@users.noreply.github.com>
2022-11-06 01:09:10 -0700

    Add files via upload

---
+++ configuration/config.lua
@@ -0,0 +1,56 @@
+-----------------For support, scripts, and more----------------
+--------------- https://discord.gg/wasabiscripts  -------------
+---------------------------------------------------------------
+
+local seconds, minutes = 1000, 60000
+Config = {}
+
+Config.checkForUpdates = true -- Check for updates?
+Config.oldESX = false -- Using ESX 1.1 or older put true
+
+Config.sellShop = {
+    enabled = true,
+    coords = vec3(-1612.19, -989.18, 13.01-0.9), -- X, Y, Z Coords of where fish buyer will spawn
+    heading = 45.3, -- Heading of fish buyer ped
+    ped = 'cs_old_man2' -- Ped name here
+}
+
+Config.bait = {
+    itemName = 'fishbait', -- Item name of bait
+    loseChance = 65 -- Chance of loosing bait(Setting to 100 will use bait every cast)
+}
+
+Config.fishingRod = {
+    itemName = 'fishingrod', -- Item name of fishing rod
+    breakChance = 25 --Chance of breaking pole when failing skillbar (Setting to 0 means never break)
+}
+
+Config.timeForBite = { -- Set min and max random range of time it takes for fish to be on the line.
+    min = 2 * seconds,
+    max = 20 * seconds
+}
+
+Config.fish = {
+    { item = 'tuna', price = {300, 550}, difficulty = {'medium', 'easy', 'easy'} }, -- name is the item name of the fish(must be in DB of items) / Price is the range of price it will sell to fish buyer / difficulty is how many & how hard skillcheck is
+    { item = 'salmon', price = {235, 300}, difficulty = {'medium', 'easy'} },
+    { item = 'trout', price = {190, 235}, difficulty = {'easy', 'easy'} },
+    { item = 'anchovy', price = {100, 190}, difficulty = {'easy'} },
+}
+
+RegisterNetEvent('wasabi_fishing:notify')
+AddEventHandler('wasabi_fishing:notify', function(title, message, msgType)
+    -- Place notification system info here, ex: exports['mythic_notify']:SendAlert('inform', message)
+    if not msgType then
+        lib.notify({
+            title = title,
+            description = message,
+            type = 'inform'
+        })
+    else
+        lib.notify({
+            title = title,
+            description = message,
+            type = msgType
+        })
+    end
+end)
---
+++ configuration/strings.lua
@@ -0,0 +1,31 @@
+-----------------For support, scripts, and more----------------
+--------------- https://discord.gg/wasabiscripts  -------------
+---------------------------------------------------------------
+Strings = {
+    intro_instruction = 'Press ~INPUT_ATTACK~ to cast line, ~INPUT_FRONTEND_RRIGHT~ to cancel.',
+    rod_broke = 'Rod Snapped',
+    rod_broke_desc = 'You pulled to hard and your fishing rod snapped!',
+    cannot_perform = 'Action Incomplete',
+    cannot_perform_desc = 'You cannot do this right now.',
+    failed = 'Failed',
+    failed_fish = 'You failed to catch fish!',
+    no_water = 'No Water',
+    no_water_desc = 'You are not facing any water.',
+    no_bait = 'Missing Bait',
+    no_bait_desc = 'You don\'t have fishing bait.',
+    bait_lost = 'Bait Lost',
+    bait_lost_desc = 'Fishing bait was lost.',
+    fish_success = 'Caught Fish',
+    fish_success_desc = 'You caught a %s!',
+    sell_shop_blip = 'Fish Market',
+    sell_fish = '[E] - Sell Fish',
+    kicked = 'Nice try, please do not attempt to exploit!',
+    sold_for = 'Sold Fish',
+    sold_for_desc = 'You sold %sx %s for $%s',
+    got_bite = 'Bite Detected',
+    got_bite_desc = 'Your pole has a fish on the line, get ready!',
+    waiting_bite = 'Awaiting Bite',
+    waiting_bite_desc = 'Please wait for a fish to bite your hook.',
+    cannot_carry = 'Cannot Carry',
+    cannot_carry_desc = 'You cannot carry reward!'
+}
