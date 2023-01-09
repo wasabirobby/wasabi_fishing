201ec51d3c83b5d036b9b5a2cacce1438e3982d6
wasabirobby <79250053+wasabirobby@users.noreply.github.com>
2023-01-08 23:43:42 -0800

    feat: qbcore support

--- server/server.lua
+++ server/server.lua
@@ -1,13 +1,8 @@
 -----------------For support, scripts, and more----------------
 --------------- https://discord.gg/wasabiscripts  -------------
 ---------------------------------------------------------------
-
-ESX = exports['es_extended']:getSharedObject()
-
-
 lib.callback.register('wasabi_fishing:checkItem', function(source, itemname)
-    local xPlayer = ESX.GetPlayerFromId(source)
-    local item = xPlayer.getInventoryItem(itemname).count
+    local item = HasItem(source, itemname)
     if item >= 1 then
         return true
     else
@@ -22,78 +17,59 @@ end)
 
 RegisterServerEvent('wasabi_fishing:rodBroke')
 AddEventHandler('wasabi_fishing:rodBroke', function()
-    local xPlayer = ESX.GetPlayerFromId(source)
-    xPlayer.removeInventoryItem(Config.fishingRod.itemName, 1)
+    RemoveItem(source, Config.fishingRod.itemName, 1)
     TriggerClientEvent('wasabi_fishing:interupt', source)
 end)
 
 RegisterServerEvent('wasabi_fishing:tryFish')
 AddEventHandler('wasabi_fishing:tryFish', function(data)
-    local xPlayer = ESX.GetPlayerFromId(source)
-    local xPole = xPlayer.getInventoryItem(Config.fishingRod.itemName).count
-    local xBait = xPlayer.getInventoryItem(Config.bait.itemName).count
+    local xPole = HasItem(source, Config.fishingRod.itemName)
+    local xBait = HasItem(source, Config.bait.itemName)
     if xPole > 0 and xBait > 0 then
         local chance = math.random(1,100)
         if chance <= Config.bait.loseChance then
-            xPlayer.removeInventoryItem(Config.bait.itemName, 1)
+            RemoveItem(source, Config.bait.itemName, 1)
             TriggerClientEvent('wasabi_fishing:notify', source, Strings.bait_lost, Strings.bait_lost_desc, 'error')
         end
-        local awardItem = data.item
-        local awardLabel = ESX.GetItemLabel(awardItem)
-        if Config.OldESX then
-            local limitItem = xPlayer.getInventoryItem(awardItem)
-            if limitItem.limit == -1 or (limitItem.count + 1) <= limitItem.limit then
-                xPlayer.addInventoryItem(awardItem, 1)
-                TriggerClientEvent('wasabi_fishing:notify', source, Strings.fish_success, string.format(Strings.fish_success_desc, awardLabel), 'success')
-            else
-                TriggerClientEvent('wasabi_fishing:notify', source, Strings.cannot_carry, Strings.cannot_carry_desc, 'error')
-            end
-        else
-            if xPlayer.canCarryItem(awardItem, 1) then
-                xPlayer.addInventoryItem(awardItem, 1)
-                TriggerClientEvent('wasabi_fishing:notify', source, Strings.fish_success, string.format(Strings.fish_success_desc, awardLabel), 'success')
-            else
-                TriggerClientEvent('wasabi_fishing:notify', source, Strings.cannot_carry, Strings.cannot_carry_desc, 'error')
-            end
-        end
+        AddItem(source, data.item, 1)
+        TriggerClientEvent('wasabi_fishing:notify', source, Strings.fish_success, string.format(Strings.fish_success_desc, data.label), 'success')
     elseif xPole > 0 and xBait < 1 then
         TriggerClientEvent('wasabi_fishing:interupt', source)
         TriggerClientEvent('wasabi_fishing:notify', source, Strings.no_bait, Strings.no_bait_desc, 'error')
     elseif xPole < 1 then
-        xPlayer.kick(Strings.kicked)
+        KickPlayer(source, Strings.kicked)
     end
 end)
 
 RegisterServerEvent('wasabi_fishing:sellFish')
 AddEventHandler('wasabi_fishing:sellFish', function()
-    local xPlayer = ESX.GetPlayerFromId(source)
     local playerPed = GetPlayerPed(source)
     local playerCoord = GetEntityCoords(playerPed)
     local distance = #(playerCoord - Config.sellShop.coords)
     if distance == nil then
-        xPlayer.kick(Strings.kicked)
+        KickPlayer(source, Strings.kicked)
         return
     end
     if distance > 3 then
-        xPlayer.kick(Strings.kicked)
+        KickPlayer(source, Strings.kicked)
         return
     end
     for i=1, #Config.fish do
-        if xPlayer.getInventoryItem(Config.fish[i].item).count > 0 then
+        if HasItem(source, Config.fish[i].item) > 0 then
             local rewardAmount = 0
-            for j=1, xPlayer.getInventoryItem(Config.fish[i].item).count do
+            for j=1, HasItem(source, Config.fish[i].item) do
                 rewardAmount = rewardAmount + math.random(Config.fish[i].price[1], Config.fish[i].price[2])
             end
             if rewardAmount > 0 then
-                xPlayer.addMoney(rewardAmount)
-                TriggerClientEvent('wasabi_fishing:notify', source, Strings.sold_for, (Strings.sold_for_desc):format(xPlayer.getInventoryItem(Config.fish[i].item).count, xPlayer.getInventoryItem(Config.fish[i].item).label, addCommas(rewardAmount)), 'success')
-                xPlayer.removeInventoryItem(Config.fish[i].item, xPlayer.getInventoryItem(Config.fish[i].item).count)
+                AddMoney(source, 'money', rewardAmount)
+                TriggerClientEvent('wasabi_fishing:notify', source, Strings.sold_for, (Strings.sold_for_desc):format(HasItem(source, Config.fish[i].item), Config.fish[i].label, addCommas(rewardAmount)), 'success')
+                RemoveItem(source, Config.fish[i].item, HasItem(source, Config.fish[i].item))
             end
         end
     end
 end)
 
-ESX.RegisterUsableItem(Config.fishingRod.itemName, function(source)
+RegisterUsableItem(Config.fishingRod.itemName, function(source)
     TriggerClientEvent('wasabi_fishing:startFishing', source)
 end)
 
