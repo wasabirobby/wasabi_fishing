35e8822179ef4a39b99fb52f5f231675e1b8f49e
wasabirobby <79250053+wasabirobby@users.noreply.github.com>
2023-01-08 23:44:15 -0800

    chore: formatting, version bump / feat: qbcore support

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -6,25 +6,14 @@ fx_version 'cerulean'
 game 'gta5'
 lua54 'yes'
 
-description 'Wasabi ESX Skill Based Fishing'
+version '2.0.3'
 author 'wasabirobby#5110'
+description 'Wasabi ESX/QBCore Skill Based Fishing'
 
-version '2.0.2'
+shared_scripts { '@ox_lib/init.lua', 'configuration/*.lua' }
 
-client_scripts {
-	'client/*.lua'
-}
+client_scripts { 'bridge/**/client.lua', 'client/*.lua' }
 
-server_scripts {
-	'server/*.lua'
-}
+server_scripts { 'bridge/**/server.lua', 'server/*.lua' }
 
-shared_scripts {
-    '@ox_lib/init.lua',
-    'configuration/*.lua'
-}
-
-dependencies {
-    'es_extended',
-    'ox_lib'
-}
+dependencies { 'ox_lib' }
