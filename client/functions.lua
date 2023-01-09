91585b0d3b1cdec7ce82f9f2a81f00d11877ff35
wasabirobby <79250053+wasabirobby@users.noreply.github.com>
2023-01-08 23:42:54 -0800

    chore: formatting

--- client/functions.lua
+++ client/functions.lua
@@ -2,20 +2,20 @@
 --------------- https://discord.gg/wasabiscripts  -------------
 ---------------------------------------------------------------
 
-showHelp = function(msg)
+ShowHelp = function(msg)
     BeginTextCommandDisplayHelp('STRING')
     AddTextComponentSubstringPlayerName(msg)
     EndTextCommandDisplayHelp(0, false, true, -1)
 end
 
-waterCheck = function()
+WaterCheck = function()
     local headPos = GetPedBoneCoords(cache.ped, 31086, 0.0, 0.0, 0.0)
     local offsetPos = GetOffsetFromEntityInWorldCoords(cache.ped, 0.0, 50.0, -25.0)
     local water, waterPos = TestProbeAgainstWater(headPos.x, headPos.y, headPos.z, offsetPos.x, offsetPos.y, offsetPos.z)
     return water, waterPos
 end
 
-createBlip = function(coords, sprite, colour, text, scale)
+CreateBlip = function(coords, sprite, colour, text, scale)
     local blip = AddBlipForCoord(coords)
     SetBlipSprite(blip, sprite)
     SetBlipColour(blip, colour)
@@ -27,8 +27,8 @@ createBlip = function(coords, sprite, colour, text, scale)
     return blip
 end
 
-tryFish = function(data)
-    TriggerServerEvent('wasabi_fishing:tryFish', data)
+TryFish = function(data)
+    TriggerServerEvent('wasabi_fishing:TryFish', data)
 end
 
 FishingSellItems = function()
